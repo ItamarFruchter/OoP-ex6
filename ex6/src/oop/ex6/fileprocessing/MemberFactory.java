fccfa4fbcd336b6408093f8194f4ba3a868ac2f5
Ohad Ben-Or <ohadbenor1996@gmail.com>
2016-06-14 00:26:30 +0300

    finished member factory

--- ex6/src/oop/ex6/fileprocessing/MemberFactory.java
+++ ex6/src/oop/ex6/fileprocessing/MemberFactory.java
@@ -1,27 +1,54 @@
 package oop.ex6.fileprocessing;
 
+import java.util.LinkedList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import oop.ex6.error.IllegalCodeException;
 import oop.ex6.variables.Member;
+import oop.ex6.variables.NonValidValueException;
 
 
 public class MemberFactory {
 
 	private static final String EMPTY_STRING = "";
+	private static final char SPACE_CHAR = ' ';
 	private static final Pattern SPACES = Pattern.compile("\\s+");
 	private static final Pattern WORD = Pattern.compile("\\w+");
-	private static final Pattern VALUE = Pattern.compile("[\\w\"\\.']+");
 	private static final Pattern WITH_MODIFIER = Pattern.compile("\\w+\\s+\\w+\\s+\\w+");
+	private static final int MEMBER_VALUE = 1;
+	private static final int MEMBER_NAME = 0;
 	
+	
+	private static class MemberParameters{
+		
+		private String name;
+		private String value;
+		
+		/**
+		 * constructs the member parameters
+		 * @param name - the name of the object
+		 * @param value - the value of the object
+		 */
+		private MemberParameters(String name, String value){
+			this.name = name;
+			this.value = value;
+		}
+		
+		public String getName(){
+			return name;
+		}
+		
+		public String getValue(){
+			return value;
+		}
+		
+	}
 	
 	private MemberFactory(){}
 	
-	public static Member[] createMembers(String line){
+	public static Member[] createMembers(String line) throws NonValidValueException, IllegalCodeException{
 		String tempString = new String(line);
-		String modifier = null;
-		String type = null;
-		String name = null;
-		String value = null;
 		clearSpaces(tempString);
 		Matcher withModifireMatcher = WITH_MODIFIER.matcher(tempString);
 		if (withModifireMatcher.lookingAt()){
@@ -30,59 +57,55 @@ public class MemberFactory {
 		else {
 			return createMembersWithoutModifire(tempString);
 		}
-		
-		wordMatcher.lookingAt();
-		if (tempString.substring(wordMatcher.start(), wordMatcher.end()).equals(MODIFIER)){
-			modifier = true;
-			wordMatcher.replaceFirst(EMPTY_STRING);
-			wordMatcher.reset();
-			wordMatcher.lookingAt();
-			type = tempString.substring(wordMatcher.start(), wordMatcher.end());
-			wordMatcher.replaceFirst(EMPTY_STRING);
-		}
-		else {
-			type = tempString.substring(wordMatcher.start(), wordMatcher.end());
-			wordMatcher.replaceFirst(EMPTY_STRING);
-		}
-		Matcher declerationMatcher = DECLERATION.matcher(tempString);
-		while (declerationMatcher.find()){
-			String decleration = tempString.substring(wordMatcher.start(), wordMatcher.end());
-			wordMatcher.reset(decleration);
-			wordMatcher.find();
-			name = decleration.substring(wordMatcher.start(), wordMatcher.end());
-			wordMatcher.replaceFirst(EMPTY_STRING);
-			Matcher valueMatcher = VALUE.matcher(decleration);
-			if (valueMatcher.find()){
-				value = decleration.substring(valueMatcher.start(), valueMatcher.end());
-				listOfObjects.add(new ObjectParameters(modifier, type, name, value));
-				name = null;
-				value = null;
-				declerationMatcher.replaceFirst(EMPTY_STRING);
-				declerationMatcher.reset();
-			}
-			listOfObjects.add(new ObjectParameters(modifier, type, name, value));
-			name = null;
-			declerationMatcher.replaceFirst(EMPTY_STRING);
-			declerationMatcher.reset();
-		}
-		return null
-	}
+	}	
 	
-	private static Member[] createMembersWithoutModifire(String tempString) {
-		// TODO Auto-generated method stub
-		return null;
+	private static Member[] createMembersWithoutModifire(String tempString) throws NonValidValueException, IllegalCodeException {
+		LinkedList<Member> listOfMembers = new LinkedList<Member>();
+		Matcher wordMatcher = WORD.matcher(tempString);
+		wordMatcher.find();
+		String type = tempString.substring(wordMatcher.start(), wordMatcher.end());
+		wordMatcher.replaceFirst(EMPTY_STRING);
+		MemberParameters[] members = extractMembers(tempString);
+		for (MemberParameters member : members ){
+			listOfMembers.add(new Member(member.getName(), type, member.getValue()));
+		}
+		return listOfMembers.toArray(new Member[listOfMembers.size()]);
 	}
 
-	private static Member[] createMembersWithModifire(String tempString) {
+	private static Member[] createMembersWithModifire(String tempString) throws NonValidValueException, IllegalCodeException {
+		LinkedList<Member> listOfMembers = new LinkedList<Member>();
 		Matcher wordMatcher = WORD.matcher(tempString);
 		wordMatcher.find();
 		String modifier = tempString.substring(wordMatcher.start(), wordMatcher.end());
 		wordMatcher.replaceFirst(EMPTY_STRING);
 		wordMatcher.reset(tempString);
 		String type = tempString.substring(wordMatcher.start(), wordMatcher.end());
-		
-		return null;
+		wordMatcher.replaceFirst(EMPTY_STRING);
+		MemberParameters[] members = extractMembers(tempString);
+		for (MemberParameters member : members ){
+			listOfMembers.add(new Member(member.getName(), type, member.getValue(), modifier));
+		}
+		return listOfMembers.toArray(new Member[listOfMembers.size()]);
 	}
+	
+	private static MemberParameters[] extractMembers(String tempString) {
+		LinkedList<MemberParameters> members = new LinkedList<MemberParameters>();
+		String[] membersString = tempString.split(",");
+		for (int i = 0; i < membersString.length; i++){
+			if (i == membersString.length-1){
+				membersString[i].replace(';', SPACE_CHAR);
+			}
+			String[] parameters = membersString[i].split("=");
+			if (parameters.length == 2){
+				members.add(new MemberParameters(parameters[MEMBER_NAME], parameters[MEMBER_VALUE]));
+			}
+			else {
+				members.add(new MemberParameters(parameters[MEMBER_NAME], null));
+			}
+		}
+		return members.toArray(new MemberParameters[members.size()]);
+	}
+
 
 	private static void clearSpaces(String stringToWork){
 		Matcher spacesMatcher = SPACES.matcher(stringToWork);
