027fc4c63d093588d320cb32c3ac3f08e986af55
T8143777 <T8143777@talpiot132.talpiot.huji.ac.il>
2016-06-13 20:42:01 +0300

    First commit!

---
+++ ex6/src/oop/ex6/variables/IllegalNameException.java
@@ -0,0 +1,10 @@
+package oop.ex6.variables;
+
+import oop.ex6.error.IllegalCodeException;
+
+/**
+ * Arises whenever a illegal name is given.
+ */
+public class IllegalNameException extends IllegalCodeException {
+
+}
---
+++ ex6/src/oop/ex6/variables/InvalidTypeException.java
@@ -0,0 +1,10 @@
+package oop.ex6.variables;
+
+import oop.ex6.error.IllegalCodeException;
+
+/**
+ * Arises whenever an invalid type is entered.
+ */
+public class InvalidTypeException extends IllegalCodeException {
+	
+}
