404dea586ff6e5186c086b45f795131816bde835
Ohad Ben-Or <ohadbenor1996@gmail.com>
2016-06-13 21:20:21 +0300

    fixed illegal

--- ex6/src/oop/ex6/error/IllegalCodeException.java
+++ ex6/src/oop/ex6/error/IllegalCodeException.java
@@ -1,5 +1,10 @@
-package src.oop.ex6.error;
+package oop.ex6.error;
 
 public class IllegalCodeException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 }
